1cea303244a4533fd0628479d8aa803602cf3981
najmabegum <naj.sheikh94@gmail.com>
2021-06-09 21:27:35 +0200

    Ones matrix multiplication

--- SealMVDemo/MVDenseMatrix.cpp
+++ SealMVDemo/MVDenseMatrix.cpp
@@ -326,7 +326,7 @@ void MVDenseMatrix(int dimension, bool rescale)
 
 	vector<double> expectedvector1 = get_Linear_Transformation_expected_vector(dimension, pod_matrix1_set2, pod_matrix1_set2[0]);
 	vector<double> errornorm1result(dimension);
-	get_maximum_error_norm(output_result_set2, expectedvector1, dimension, errornorm1result);
+	get_max_error_norm(output_result_set2, expectedvector1, dimension, errornorm1result);
 
 	// ------------- SECOND COMPUTATION ----------------
 	outf << "# index 1" << endl;
@@ -360,7 +360,7 @@ void MVDenseMatrix(int dimension, bool rescale)
 	test_Linear_Transformation(dimension, pod_matrix1_set2, pod_matrix1_set2[0]);
 	vector<double> expectedvector2 = get_Linear_Transformation_expected_vector(dimension, pod_matrix1_set2, pod_matrix1_set2[0]);
 	vector<double> errornorm2result(dimension);
-	get_maximum_error_norm(output_result2_set2, expectedvector2, dimension, errornorm2result);
+	get_max_error_norm(output_result2_set2, expectedvector2, dimension, errornorm2result);
 
 	outf.close();
 }
